c673a26f09bfcee3c18eb14cf99ffc7ff8640dcc
thales <thalesdaniel2013@gmail.com>
2022-05-24 18:45:43 -0300

    feat: add express.json

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,17 @@ import express from 'express'
 
 const app = express()
 
+// req middlewares
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// routes
+
 app.get('/', (req, res) => {
 	res.send('Hello World')
 })
+
+// res middlewares
 
 export default app
--- src/api/server.ts
+++ src/api/server.ts
@@ -1,6 +1,7 @@
 import app from '../api'
-import { config } from 'dotenv'
-config()
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const port = process.env.API_PORT || 3001
 
